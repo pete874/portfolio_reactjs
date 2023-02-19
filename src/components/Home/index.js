7ad9be4180bea916e5e71caea663304ec9da4812
Petri Nurmi <pete874@hotmail.com>
2023-02-19 21:24:13 +0200

    .

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
+import LogoTitle from "../../assets/images/plogo-s.png";
+import './index.scss';
+
 const Home = () => {
 
 
     return (
         <div className="container home-page">
             <div className="text-zone">
+                <h1>Hi, <br/> I'm
+                <img src={LogoTitle} alt="developer" />
+                etri Nurmi
+                <br/>
+                Software developer
+                </h1>
+                <h2>Frontend developer</h2>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
             </div>
 
         </div>
