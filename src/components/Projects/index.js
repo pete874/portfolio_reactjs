626be429addb8f75c2bc9eb89698d28aaf35554d
Petri Nurmi <pete874@hotmail.com>
2023-02-25 17:14:32 +0200

    .

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,6 +5,7 @@ import Project3 from '../../assets/images/project3.jpg'
 import Project4 from '../../assets/images/project4.jpg'
 import Project5 from '../../assets/images/project5.jpg'
 import Project6 from '../../assets/images/project6.jpg'
+import Project7 from '../../assets/images/project7.jpg'
 import { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 
@@ -17,7 +18,7 @@ const Projects = () => {
     useEffect(() => {
         const timer = setTimeout(() => {
             setProjectClass("hover-items1")
-        }, 3000);
+        }, 2000);
     }, [])
     
     return (
@@ -32,7 +33,7 @@ const Projects = () => {
                     <div class={projectClass} >
                         <p>My first Portfoliopage</p>
                         <a href='https://github.com/pete874/First_portfoliopage' target={'_blank'}>Github</a> <br></br>
-                        <a>Link</a>
+                        <a href='https://pete874.github.io/First_portfoliopage/' target={'_blank'}>Link</a>
                     </div>
                 </div>
             <div className='project'>
@@ -40,7 +41,7 @@ const Projects = () => {
                     <div class={projectClass}>
                         <p>Tic-Tac-Toe using Javascript</p>
                         <a href='https://github.com/pete874/ristinolla_js' target={'_blank'}>Github</a> <br></br>
-                        <a>Link</a>
+                        <a href='https://pete874.github.io/ristinolla_js/' target={'_blank'}>Link</a>
                     </div>
             </div>
             <div className='project'>
@@ -48,7 +49,7 @@ const Projects = () => {
                     <div class={projectClass}>
                         <p>Festival page with registration form</p>
                         <a href='https://github.com/pete874/yhteistyo/tree/main/projekti_3-4' target={'_blank'}>Github</a> <br></br>
-                        <a>Link</a>
+                        <a href='https://pete874.github.io/yhteistyo/projekti_3-4/' target={'_blank'}>Link</a>
                     </div>                
             </div>
             <div className='project'>
@@ -56,7 +57,7 @@ const Projects = () => {
                     <div class={projectClass}>
                         <p>Calculator with Javascript</p>
                         <a href='https://github.com/pete874/yhteistyo/tree/main/laskin' target={'_blank'}>Github</a> <br></br>
-                        <a>Link</a>
+                        <a href='https://pete874.github.io/yhteistyo/laskin/' target={'_blank'}>Link</a>
                     </div>       
             </div>
             <div className='project'>
@@ -64,7 +65,7 @@ const Projects = () => {
                     <div class={projectClass}>
                         <p>To Do list with Javascript</p>
                         <a href='https://github.com/pete874/yhteistyo/tree/main/Tehtavalista' target={'_blank'}>Github</a> <br></br>
-                        <a>Link</a>
+                        <a href='https://pete874.github.io/yhteistyo/Tehtavalista/' target={'_blank'}>Link</a>
                     </div> 
                       
             </div>
@@ -73,7 +74,15 @@ const Projects = () => {
                     <div class={projectClass}>
                         <p>Countdown timer using Javascript</p>
                         <a href='https://github.com/pete874/countdown-timer' target={'_blank'}>Github</a> <br></br>
-                        <a>Link</a>
+                        <a href='https://pete874.github.io/countdown-timer/' target={'_blank'}>Link</a>
+                    </div>  
+            </div>
+            <div className='project'>
+                <img src={Project7} id="project7" alt="P"></img>  
+                    <div class={projectClass}>
+                        <p>Countdown timer using Javascript</p>
+                        <a href='https://github.com/pete874/yhteistyo/tree/main/arvauspeli' target={'_blank'}>Github</a> <br></br>
+                        <a href='https://pete874.github.io/yhteistyo/arvauspeli/' target={'_blank'}>Link</a>
                     </div>  
             </div>
             </div>
