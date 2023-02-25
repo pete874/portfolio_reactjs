f56332f3afadc5635544ee28ca199ab76757acc0
Petri Nurmi <pete874@hotmail.com>
2023-02-25 15:10:28 +0200

    .

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -5,16 +5,29 @@ import Project3 from '../../assets/images/project3.jpg'
 import Project4 from '../../assets/images/project4.jpg'
 import Project5 from '../../assets/images/project5.jpg'
 import Project6 from '../../assets/images/project6.jpg'
+import { useEffect, useState } from 'react'
 
 const Projects = () => {
+
+        //usestate for projectclass
+        const [projectClass, setProjectClass] = useState('hover-items')
+
+        //classname change on state change and 3sec delay. 
+        useEffect(() => {
+            const timer = setTimeout(() => {
+                setProjectClass("hover-items1")
+            }, 3000);
+        }, [])
     return (
+
+        
 
         //project divs
         <div className='projects-page'>
             <div className='projects-container'>
                 <div className='project' id="project1">
                     <img src={Project1}  alt="P"></img>
-                    <div class="hover-items" >
+                    <div class={projectClass} >
                         <p>My first Portfoliopage</p>
                         <a href='https://github.com/pete874/First_portfoliopage' target={'_blank'}>Github</a> <br></br>
                         <a>Link</a>
@@ -22,7 +35,7 @@ const Projects = () => {
                 </div>
             <div className='project'>
                 <img src={Project2} id="project2" alt="P"></img>
-                    <div class="hover-items">
+                    <div class={projectClass}>
                         <p>Tic-Tac-Toe using Javascript</p>
                         <a href='https://github.com/pete874/ristinolla_js' target={'_blank'}>Github</a> <br></br>
                         <a>Link</a>
@@ -30,7 +43,7 @@ const Projects = () => {
             </div>
             <div className='project'>
                 <img src={Project3} id="project3" alt="P"></img>
-                    <div class="hover-items">
+                    <div class={projectClass}>
                         <p>Festival page with registration form</p>
                         <a href='https://github.com/pete874/yhteistyo/tree/main/projekti_3-4' target={'_blank'}>Github</a> <br></br>
                         <a>Link</a>
@@ -38,7 +51,7 @@ const Projects = () => {
             </div>
             <div className='project'>
                 <img src={Project4} id="project4" alt="P"></img>
-                    <div class="hover-items">
+                    <div class={projectClass}>
                         <p>Calculator with Javascript</p>
                         <a href='https://github.com/pete874/yhteistyo/tree/main/laskin' target={'_blank'}>Github</a> <br></br>
                         <a>Link</a>
@@ -46,7 +59,7 @@ const Projects = () => {
             </div>
             <div className='project'>
                 <img src={Project5} id="project5" alt="P"></img>
-                    <div class="hover-items">
+                    <div class={projectClass}>
                         <p>To Do list with Javascript</p>
                         <a href='https://github.com/pete874/yhteistyo/tree/main/Tehtavalista' target={'_blank'}>Github</a> <br></br>
                         <a>Link</a>
@@ -55,7 +68,7 @@ const Projects = () => {
             </div>
             <div className='project'>
                 <img src={Project6} id="project6" alt="P"></img>  
-                    <div class="hover-items">
+                    <div class={projectClass}>
                         <p>Countdown timer using Javascript</p>
                         <a href='https://github.com/pete874/countdown-timer' target={'_blank'}>Github</a> <br></br>
                         <a>Link</a>
